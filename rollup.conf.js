50163a3be84e607e97bb8ad19cc67b48e95e11d0
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2020-08-01 18:28:38 +0700

    Bump the version to 0.4.3

--- rollup.conf.js
+++ rollup.conf.js
@@ -2,7 +2,7 @@ let bannerText = `
 // ==UserScript==
 // @name         DTF: Navigate with keys
 // @namespace    http://tampermonkey.net/
-// @version      0.4.2
+// @version      0.4.3
 // @description  Navigate with keys!
 // @author       mr-m
 // @match        *://dtf.ru/*
