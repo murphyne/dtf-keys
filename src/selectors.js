3c2885bc9c60727b4fb4d4be8761086850ea071a
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2021-11-16 00:26:07 +0700

    Delete outdated selectors

--- src/selectors.js
+++ src/selectors.js
@@ -1,12 +1,5 @@
 export {
-  selectorVacancyHeader,
-  selectorCustomSubsite_html,
-  selectorSubsiteCover,
-  selectorSubsiteHead,
   selectorNewsWidget,
-  selectorTeaserPodcast,
-  selectorDailyPromoUnit,
-  selectorBlogsEntries,
   selectorVacanciesWidget,
   selectorWidgetWrapper,
   selectorFeedItem,
@@ -14,27 +7,13 @@ export {
   selectElements,
 };
 
-const selectorVacancyHeader = '.vacancy_header';
-const selectorCustomSubsite_html = '.custom_subsite_html:first-of-type';
-const selectorSubsiteCover = '.subsite__cover:first-of-type';
-const selectorSubsiteHead = '.subsite_head:first-of-type';
 const selectorNewsWidget = '.news_widget';
-const selectorTeaserPodcast = '.teaserPodcast';
-const selectorDailyPromoUnit = '.daily-promo-unit';
-const selectorBlogsEntries = '.island.island--expanded.island--blogs_entries';
 const selectorVacanciesWidget = '.vacancies_widget';
 const selectorWidgetWrapper = '.widget_wrapper';
 const selectorFeedItem = '.content-feed';
 
 const selectorComposite = [
-  selectorVacancyHeader,
-  selectorCustomSubsite_html,
-  selectorSubsiteCover,
-  selectorSubsiteHead,
   selectorNewsWidget,
-  selectorTeaserPodcast,
-  selectorDailyPromoUnit,
-  selectorBlogsEntries,
   selectorVacanciesWidget,
   selectorWidgetWrapper,
   selectorFeedItem,
