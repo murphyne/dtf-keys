4fc4b89a9ff599e2d11df70fe2b3c1975a602408
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2022-11-13 13:46:23 +0600

    Add selector for page headers

--- src/selectors.js
+++ src/selectors.js
@@ -1,5 +1,7 @@
 export {
   selectorNewsWidget,
+  selectorSubsiteHeader,
+  selectorServicesHeader,
   selectorVacanciesWidget,
   selectorWidgetWrapper,
   selectorFeedItem,
@@ -8,12 +10,16 @@ export {
 };
 
 const selectorNewsWidget = '.news-widget,.news-widget-skeleton'; // Блок новостей за день
+const selectorSubsiteHeader = '.subsite-header'; // Блок «Шапка подсайта»
+const selectorServicesHeader = '.services-header'; // Блок «Шапка поиска»
 const selectorVacanciesWidget = '.vacancies_widget'; // Блок «Вакансии»
 const selectorWidgetWrapper = '.widget_wrapper'; // Блок «Лучшие комментарии»
 const selectorFeedItem = '.content-feed'; // Посты
 
 const selectorComposite = [
   selectorNewsWidget,
+  selectorSubsiteHeader,
+  selectorServicesHeader,
   selectorVacanciesWidget,
   selectorWidgetWrapper,
   selectorFeedItem,
