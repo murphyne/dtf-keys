2d3e69f7c0c587416df3b160f7dcd0d3b4aa3755
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2024-04-08 12:44:58 +0400

    Update selectors after the redesign

--- src/selectors.js
+++ src/selectors.js
@@ -14,14 +14,14 @@ export {
 
 const selectorFeedHeader = '.feed_header';
 const selectorNewEntries = '.new_entries';
-const selectorSiteHeader = '.site-header';
-const selectorContentLink = 'a.content-link';
-const selectorTextInput = '.v-text-input__input';
+const selectorSiteHeader = '.header';
+const selectorContentLink = 'a.content__link';
+const selectorTextInput = '.text-input';
 
 const selectorNewsWidget = '.news-widget,.news-widget-skeleton'; // Блок новостей за день
 const selectorSubsiteHeader = '.subsite-header'; // Блок «Шапка подсайта»
-const selectorServicesHeader = '.services-header'; // Блок «Шапка поиска»
-const selectorFeedItem = '.content-feed'; // Посты
+const selectorServicesHeader = '.discovery__header'; // Блок «Шапка поиска»
+const selectorFeedItem = '.content:not(.content--embed)'; // Посты
 
 const selectorComposite = [
   selectorNewsWidget,
