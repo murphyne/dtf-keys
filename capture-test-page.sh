cd595ac892467b353742af2a69aed0db44615cfc
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2024-04-08 14:43:25 +0400

    Lock version of SingleFile docker image

--- capture-test-page.sh
+++ capture-test-page.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm capsulecode/singlefile "https://dtf.ru" > test/DTF-SingleFile.html
+docker run --rm capsulecode/singlefile:v0.3.32 "https://dtf.ru" > test/DTF-SingleFile.html
